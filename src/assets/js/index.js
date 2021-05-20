9caddab56478c2f0eb5272072e337892a195356d
karthik2804 <raams.karthik@gmail.com>
2021-05-20 22:04:08 +0530

    added themer

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,11 +1,41 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
-console.log(hamburger)
 hamburger.addEventListener('click', ()=>{
     console.log("here")
-    //themepicker.classList.remove("is-open")
     navLinks.classList.toggle("open");
-    //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
+
+const themepicker = document.getElementById("themepicker")
+const ThemeSwitch = document.getElementById("themeSwitch")
+let themes = ["default", "dark", "dark2", "light2"]
+
+let currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (themes.includes(currentTheme)) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+}
+else {
+    let prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (prefersDark) {
+        currentTheme = 'dark'
+    }
+    else {
+        currentTheme = "default"
+    }
+}
+
+
+function switchTheme(tag) {
+    console.log(tag)
+    document.getElementById(currentTheme).classList.remove("is-active")
+    localStorage.setItem('theme',tag); //add this
+    currentTheme = tag
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    document.getElementById(tag).classList.add("is-active")
+}
+
+function toggleThemeSelection() {
+        themepicker.classList.toggle("is-open")
+}
--- src/home/index.md
+++ src/home/index.md
@@ -6,6 +6,5 @@ layout: home.njk
 
 # Check out my <br>[Resume!](/karthik_ganeshram.pdf)
 
-I'm a computer architecture ethusiast and  enjoy web development. I write about my journey towards a better personal and professional life.
-
+I'm a computer architecture ethusiast and  enjoy web development. I write about my journey towards a better personal and professional life.  
 Contact me at [raams.karthik@gmail.com](mailto:ramms.karthik@gmail.com)
